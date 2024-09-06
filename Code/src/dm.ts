90913daf4ee216c2407a8e0397d40776c79d36df
Vladislav Maraev <vlad@maraev.me>
2024-09-06 15:07:58 +0200

    Add starter ts code (including solution)

---
+++ Code/src/dm.ts
@@ -0,0 +1,193 @@
+import { AnyActorRef, assign, createActor, setup } from "xstate";
+import { speechstate, SpeechStateExternalEvent } from "speechstate";
+import { createBrowserInspector } from "@statelyai/inspect";
+import { KEY } from "./azure";
+
+const inspector = createBrowserInspector();
+
+const azureCredentials = {
+  endpoint:
+    "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
+  key: KEY,
+};
+
+const settings = {
+  azureRegion: "northeurope",
+  azureCredentials: azureCredentials,
+  asrDefaultCompleteTimeout: 0,
+  asrDefaultNoInputTimeout: 5000,
+  locale: "en-US",
+  ttsDefaultVoice: "en-US-DavisNeural",
+};
+
+/* Grammar definition */
+interface Grammar {
+  [index: string]: { person?: string; day?: string; time?: string };
+}
+const grammar: Grammar = {
+  vlad: { person: "Vladislav Maraev" },
+  aya: { person: "Nayat Astaiza Soriano" },
+  rasmus: { person: "Rasmus Blanck" },
+  monday: { day: "Monday" },
+  tuesday: { day: "Tuesday" },
+  "10": { time: "10:00" },
+  "11": { time: "11:00" },
+};
+
+/* Helper functions */
+function isInGrammar(utterance: string) {
+  return utterance.toLowerCase() in grammar;
+}
+
+function getPerson(utterance: string) {
+  return (grammar[utterance.toLowerCase()] || {}).person;
+}
+
+interface MyDMContext extends DMContext {
+  noinputCounter: number;
+}
+interface DMContext {
+  count: number;
+  ssRef: AnyActorRef;
+}
+const dmMachine = setup({
+  types: {} as {
+    context: MyDMContext;
+    events: SpeechStateExternalEvent | { type: "CLICK" };
+  },
+  guards: {
+    noinputCounterMoreThanOne: ({ context }) => {
+      if (context.noinputCounter > 1) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  actions: {
+    /* define your actions here */
+    speechstate_prepare: ({ context }) =>
+      context.ssRef.send({ type: "PREPARE" }),
+    speechstate_listen: ({ context }) => context.ssRef.send({ type: "LISTEN" }),
+    speechstate_speak: ({ context }, params: { value: string }) =>
+      context.ssRef.send({ type: "SPEAK", value: { utterance: params.value } }),
+    debug: () => console.debug("blabla"),
+    assign_noinputCounter: assign(({ context }, params?: { value: number }) => {
+      if (!params) {
+        return { noinputCounter: context.noinputCounter + 1 };
+      }
+      return { noinputCounter: context.noinputCounter + params.value };
+    }),
+  },
+}).createMachine({
+  context: ({ spawn }) => ({
+    count: 0,
+    ssRef: spawn(speechstate, { input: settings }),
+    noinputCounter: 0,
+    // moreStuff: {thingOne: 1, thingTwo: 2}
+  }),
+  id: "DM",
+  initial: "Prepare",
+  states: {
+    Prepare: {
+      entry: [{ type: "speechstate_prepare" }],
+      on: { ASRTTS_READY: "WaitToStart" },
+    },
+    WaitToStart: {
+      on: {
+        CLICK: "PromptAndAsk",
+      },
+    },
+    PromptAndAsk: {
+      initial: "Prompt",
+      states: {
+        Prompt: {
+          entry: {
+            type: "speechstate_speak",
+            params: { value: "Hello world" },
+          },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        NoInput: {
+          initial: "Choice",
+          states: {
+            Choice: {
+              always: [
+                { guard: "noinputCounterMoreThanOne", target: "MoreThanOne" },
+                { target: "LessThanOne" },
+              ],
+            },
+            LessThanOne: {
+              entry: {
+                type: "speechstate_speak",
+                params: { value: "I didn't hear you" },
+              },
+            },
+            MoreThanOne: {
+              entry: {
+                type: "speechstate_speak",
+                params: ({ context }) => {
+                  return {
+                    value: `I didn't hear you ${context.noinputCounter} times`,
+                  };
+                },
+              },
+            },
+          },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        Ask: {
+          entry: {
+            type: "speechstate_listen",
+          },
+          on: {
+            ASR_NOINPUT: {
+              target: "NoInput",
+              actions: { type: "assign_noinputCounter" },
+            },
+            RECOGNISED: {
+              actions: [
+                {
+                  type: "speechstate_speak",
+                  params: ({ event }) => {
+                    const u = event.value[0].utterance;
+                    return {
+                      value: `You just said: ${u}. And it ${
+                        isInGrammar(u) ? "is" : "is not"
+                      } in the grammar.`,
+                    };
+                  },
+                },
+              ],
+            },
+            SPEAK_COMPLETE: "#DM.Done",
+          },
+        },
+      },
+    },
+    Done: {
+      on: {
+        CLICK: "PromptAndAsk",
+      },
+    },
+  },
+});
+
+const dmActor = createActor(dmMachine, {
+  inspect: inspector.inspect,
+}).start();
+
+dmActor.subscribe((state) => {
+  /* if you want to log some parts of the state */
+  console.debug(state.context);
+});
+
+export function setupButton(element: HTMLElement) {
+  element.addEventListener("click", () => {
+    dmActor.send({ type: "CLICK" });
+  });
+  dmActor.getSnapshot().context.ssRef.subscribe((snapshot) => {
+    const meta = Object.values(snapshot.getMeta())[0];
+    element.innerHTML = `${(meta as any).view}`;
+  });
+}
